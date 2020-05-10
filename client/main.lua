27669fd3ca148770201d65765c4e9916785888db
Henric Backman <b-d-t@msn.com>
2020-05-10 17:42:55 +0200

    checks every 3 sec for item

--- client/main.lua
+++ client/main.lua
@@ -18,38 +18,17 @@ AddEventHandler('esx:playerLoaded', function()
 	end)
 end)
 
-local runOnce, runsOnce = true, true
 Citizen.CreateThread(function()
 	while true do
 		Citizen.Wait(3000)
         hasGps(function (hasGps)
           if hasGps == true then
-				if runOnce then
-					showRadar()
-					runOnce = false
-					runsOnce = true
-				end         
+            showRadar()
           else
-			  if runsOnce then
-					hideRadar()
-					runOnce = true
-					runsOnce = false
-			  end
+            hideRadar()
           end
         end)
 	end
-end)
-
-RegisterNetEvent('esx_gps:addGPS')
-AddEventHandler('esx_gps:addGPS', function()
-	showRadar()
-	runsOnce = true
-end)
-
-RegisterNetEvent('esx_gps:removeGPS')
-AddEventHandler('esx_gps:removeGPS', function()
-	hideRadar()
-	runOnce = true
 end)
 
 function showRadar()
--- server/main.lua
+++ server/main.lua
@@ -4,23 +4,6 @@ TriggerEvent('esx:getSharedObject', function(obj)
 	ESX = obj
 end)
 
-AddEventHandler('esx:onAddInventoryItem', function(source, item, count)
-	if item.name == 'gps' then
-		TriggerClientEvent('esx_gps:addGPS', source)
-	end
-end)
-
-AddEventHandler('esx:onRemoveInventoryItem', function(source, item, count)
-	if item.name == 'gps' and item.count < 1 then
-		TriggerClientEvent('esx_gps:removeGPS', source)
-	end
-end)
-
-ESX.RegisterUsableItem('gps', function(source)
-	local xPlayer = ESX.GetPlayerFromId(source)
-	TriggerClientEvent('esx_gps:addGPS', source)
-end)
-
 ESX.RegisterServerCallback('esx_gps:checkIfHas', function(source, cb, item)
 	local xPlayer = ESX.GetPlayerFromId(source)
         local items = xPlayer.getInventoryItem(item)
